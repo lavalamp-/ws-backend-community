effb38f5cfad8ed755a9aa33a5aea84cf2754cef
lavalamp <lavalamp0x00@gmail.com>
2017-07-17 09:13:23 -0700

    Updated error in sleep call.

--- scripts/check_deployment.sh
+++ scripts/check_deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Sleeping to let Docker components start up..."
-sleep(5)
+sleep 5
 python manage.py shell -c "from lib.deploy import DeployChecker; checker = DeployChecker(); checker.print_status()"
