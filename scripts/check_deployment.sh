0f7f7124ffb230cedac24c7bd5a32ab61cf915b3
lavalamp <lavalamp0x00@gmail.com>
2017-07-17 09:09:52 -0700

    Added delay to deployment checker.

--- scripts/check_deployment.sh
+++ scripts/check_deployment.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "Sleeping to let Docker components start up..."
+sleep(5)
 python manage.py shell -c "from lib.deploy import DeployChecker; checker = DeployChecker(); checker.print_status()"
