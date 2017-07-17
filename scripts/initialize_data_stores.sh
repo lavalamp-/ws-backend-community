b054821b548438b66db438de15037341b21e296b
lavalamp <lavalamp0x00@gmail.com>
2017-07-17 09:29:23 -0700

    Updated data store init script to set user's email to verified.

--- scripts/initialize_data_stores.sh
+++ scripts/initialize_data_stores.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations rest
 python manage.py migrate
 python manage.py shell -c "from lib import bootstrap_data_stores; bootstrap_data_stores()"
 python manage.py createsuperuser
-python manage.py shell -c "from rest.models import WsUser; user = WsUser.objects.first(); user.is_active = True; user.is_enterprise_user = True; user.email_verified = True; user.save()"
+python manage.py shell -c "from rest.models import WsUser; user = WsUser.objects.first(); user.is_active = True; user.email_verified = True; user.is_enterprise_user = True; user.email_verified = True; user.save()"
